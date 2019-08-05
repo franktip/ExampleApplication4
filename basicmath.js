2bac2c05a4548f3890a45f8a6c30ae985f06cd28
franktip <f.tip@northeastern.edu>
2019-08-05 14:50:16 -0400

    more cases of named exports

---
+++ basicmath.js
@@ -0,0 +1,10 @@
+/**
+ * file: basicmath.js
+ */
+export let multiply = function (frst, second){
+	return frst * second;
+};
+
+export var exponent = function (base, exp){
+	return Math.pow(base, exp);
+};
